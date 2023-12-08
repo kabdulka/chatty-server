54f422c3783854e23902e0c4f324340e3676ddd0
kabdulka <kenabdulka@gmail.com>
2023-12-08 13:59:13 -0800

    add configurations

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,17 @@ import bodyParser from "body-parser";
 dotenv.config();
 
 const app: Express = express();
+
+app.use(express.json());
+app.use(helmet());
+
+// allow access from different servers
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "30mb"})); // extended: true
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
 const port = process.env.PORT || 9090;
 
 app.get("/", (req: Request, res: Response) => {
