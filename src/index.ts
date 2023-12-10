0560a5cc7f54b34d4ab9779f9f02645a94351a25
kabdulka <kenabdulka@gmail.com>
2023-12-09 17:00:04 -0800

    complete server setup

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,8 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import bodyParser from "body-parser";
+import OpenAI from 'openai';
+// import {Configuration, OpenAIApi} from "openi";
 
 dotenv.config();
 
@@ -18,6 +20,18 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb"})); // extended: true
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+/* openAI configuration */
+export const openai = new OpenAI({
+    apiKey: process.env.OPEN_AI_KEY, // defaults to process.env["OPENAI_API_KEY"]
+});
+
+// const configuration = new Configuration({
+//     apiKey: process.env.OPEN_AI_KEY, // defaults to process.env["OPENAI_API_KEY"]
+
+// });
+// export const openai = new OpenAIApi(configuration);
+
 
 const port = process.env.PORT || 9090;
 
