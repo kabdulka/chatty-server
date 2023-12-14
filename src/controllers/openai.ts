d328c95d68df88efd88d39756469763a00e3ecee
kabdulka <kenabdulka@gmail.com>
2023-12-14 14:11:42 -0800

    api call for ai message

--- src/controllers/openai.ts
+++ src/controllers/openai.ts
@@ -6,8 +6,28 @@ import { openai } from "..";
 const sendText = async (req, res) => {
     try {
         const { text, activeChatId } = req.body; 
-        console.log("Send Text");
-        return res.status(200).json({ text });
+        console.log("req.body: ", req.body);
+        const response = await openai.chat.completions.create({
+            model: "gpt-3.5-turbo",
+            messages: [
+                { role: "system", content: "you are a helpful assistant." },
+                { role: "user", content: text }
+            ]
+        })
+        console.log('response: ', response)
+        // api call to handle submit function
+        await axios.post(
+            `https://api.chatengine.io/chats/${activeChatId}/messages/`,
+            { text: response.choices[0].message.content },
+            {
+                headers: {
+                    "Project-ID": process.env.PROJECT_ID,
+                    "User-Name": process.env.BOT_USER_NAME,
+                    "User-Secret": process.env.BOT_USER_SECRET,
+                },
+            }
+        )
+        return res.status(200).json({ text: response.choices[0].message.content });
 
     } catch (error) {
         console.log("error", error);
