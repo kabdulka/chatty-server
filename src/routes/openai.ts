d4e385b3826efa2e395dad43aa3f3b490e75cb3e
kabdulka <kenabdulka@gmail.com>
2023-12-17 23:46:46 -0800

    add ai code response route and controller

--- src/routes/openai.ts
+++ src/routes/openai.ts
@@ -1,12 +1,13 @@
 import express, { Express } from "express";
 import dotenv from "dotenv";
 import { openai } from "../index"; // check import for openai
-import { sendText } from "../controllers/openai";
+import { sendText, sendCode } from "../controllers/openai";
 
 dotenv.config();
 
 const openaiRoutes = express.Router();
 
 openaiRoutes.post("/text", sendText);
+openaiRoutes.post("/code", sendCode);
 
 export default openaiRoutes;
